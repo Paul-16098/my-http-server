90637e8d3c390725e298084c84c79a806e006ff6
Paul-16098 <pl816098@gmail.com>
2025-08-12 19:35:32 +0800

    fix: missing generics for trait `Fn`

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,9 @@ use std::os::windows::fs::{ symlink_dir, symlink_file };
 #[cfg(not(windows))]
 use std::os::unix::fs::symlink;
 #[cfg(not(windows))]
-const symlink_dir: dyn Fn = symlink;
+const symlink_dir: dyn Fn(AsRef<Path>, AsRef<Path>) -> io::Result<()> = symlink;
 #[cfg(not(windows))]
-const symlink_file: dyn Fn = symlink;
+const symlink_file: dyn Fn(AsRef<Path>, AsRef<Path>) -> io::Result<()> = symlink;
 
 use std::path::PathBuf;
 use std::{ rc::Rc, cell::RefCell };
