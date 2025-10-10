35796c37144a4add6ddfecc792f209a7f3947629
Paul-16098 <pl816098@gmail.com>
2025-10-10 19:28:12 +0800

    feat(templating): migrate to Handlebars engine (add html-t.hbs, remove legacy template); refactor templating engine and registration

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -1,5 +1,7 @@
 //! parser
 
+use log::debug;
+
 use crate::parser::templating::set_context_value;
 
 pub(crate) mod markdown;
@@ -24,27 +26,29 @@ pub(crate) fn md2html(
   template_data_list: Vec<String>
 ) -> crate::error::AppResult<String> {
   // let c = &crate::cofg::Cofg::new();
-  let mut engine = templating::get_engine(c);
+  let mut engine = templating::get_engine(c)?;
   let mut context = templating::get_context(c);
   for template_data in template_data_list {
     set_context_value(&mut context, &template_data);
   }
-
-  let html_t = engine.compile_to_bytecode("html-t.templating")?;
+  if !engine.has_template("html-t") {
+    engine.register_template_file("html-t", "./meta/html-t.hbs")?;
+  }
 
   let ast = markdown::parser_md(md)?;
   log::trace!("ast={ast:#?}");
-  let html = &markdown_ppp::html_printer::render_html(
+  let html = markdown_ppp::html_printer::render_html(
     &ast,
     markdown_ppp::html_printer::config::Config::default()
   );
 
-  context.set_string("body", html);
-  match engine.render_compiled(&html_t, &context) {
+  context.data_mut()["body"] = handlebars::JsonValue::String(html);
+  debug!("{:#?}", engine.get_templates());
+  match engine.render_with_context("html-t", &context) {
     Ok(o) => Ok(o),
     Err(o) => {
       log::error!("md2html:{}", o);
-      Err(crate::error::AppError::Template(o))
+      Err(crate::error::AppError::RenderError(o))
     }
   }
 }
